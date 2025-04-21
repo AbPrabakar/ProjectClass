257c289d6d93f33794fc7c45ec298695484dd398
Abprabakar <abprabakar17@gmail.com>
2025-04-21 19:27:47 +0530

    praba's Code

--- src/test/java/org/test/LoginPage.java
+++ src/test/java/org/test/LoginPage.java
@@ -4,7 +4,15 @@ public class LoginPage {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("test");
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
+		
+		
+		
+		
+		
 	}
 	
 
